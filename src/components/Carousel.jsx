c908b88fb4b27274156ffbc5964ab46c0c787e75
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2023-08-14 15:22:40 +0100

    updated carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -9,8 +9,25 @@ import "swiper/css/effect-fade";
 import styles from "./stylesheets/Carousel.css";
 import projects from "../resources/projects";
 
+import image1 from "../resources/images/CarouselImages/1.jpg";
+import image2 from "../resources/images/CarouselImages/2.jpg";
+import image3 from "../resources/images/CarouselImages/3.jpg";
+import image4 from "../resources/images/CarouselImages/4.jpg";
+import image5 from "../resources/images/CarouselImages/5.jpg";
+import image6 from "../resources/images/CarouselImages/6.jpg";
+import image7 from "../resources/images/CarouselImages/7.jpg";
+
 export default function Carousel() {
   const [carouselData, setCarouselData] = useState([]);
+  const [carouselImages, setCarouselImages] = useState([
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+  ]);
 
   useEffect(() => {
     setCarouselData(projects);
@@ -28,12 +45,45 @@ export default function Carousel() {
         classname={styles.myswiper}
       >
         {carouselData.map((item) => {
+          let media = <></>;
+          if (item.image_url && item.video_url) {
+            media = (
+              <li>
+                <img src={item.image_url} alt={item.name} />
+              </li>
+            );
+          } else if (item.image_url) {
+            media = (
+              <li>
+                <img src={item.image_url} alt={item.name} />
+              </li>
+            );
+          } else if (item.video_url) {
+            media = (
+              <li>
+                <iframe
+                  width="853"
+                  height="480"
+                  src={`https://www.youtube.com/embed/${item.video_url}`}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  title="Embedded youtube"
+                />
+              </li>
+            );
+          } else {
+            media = <p>Nothing to see here...</p>;
+          }
+
           return (
             <SwiperSlide
               style={{
                 backgroundImage:
                   "linear-gradient(to left, rgba(255, 255, 255, 0.8), rgba(0, 0, 0, 0.6)), url(" +
-                  item.image_url +
+                  carouselImages[
+                    Math.floor(Math.random() * carouselImages.length)
+                  ] +
                   ")",
                 backgroundPosition: "center",
                 backgroundSize: "cover",
@@ -41,12 +91,15 @@ export default function Carousel() {
                 height: "500px",
               }}
             >
-              <div className="swiperTextbox">
-                <h1>{item.Title}</h1>
-                <h1>{item.Subtitle}</h1>
-                <Link to="/contact-us">
-                  <button className="carouselButton">Contact us</button>
-                </Link>
+              <div className="swiperContent">
+                <div className="swiperTextbox">
+                  <h1>
+                    {item.name} - {item.language}
+                  </h1>
+                  <h4>{item.description}</h4>
+                </div>
+
+                <div className="swiperContentBox">{media}</div>
               </div>
             </SwiperSlide>
           );
--- src/components/ImageDisp.jsx
+++ src/components/ImageDisp.jsx
@@ -9,30 +9,20 @@ const ImageDisp = () => {
     <ul>
       {projects.map((item) => {
         if (item.image_url && item.video_url) {
-          return (
-            <li>
-              <img src={item.image_url} alt={item.name} />
-            </li>
-          );
+          return <img src={item.image_url} alt={item.name} />;
         } else if (item.image_url) {
-          return (
-            <li>
-              <img src={item.image_url} alt={item.name} />
-            </li>
-          );
+          return <img src={item.image_url} alt={item.name} />;
         } else if (item.video_url) {
           return (
-            <li>
-              <iframe
-                width="853"
-                height="480"
-                src={`https://www.youtube.com/embed/${item.video_url}`}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="Embedded youtube"
-              />
-            </li>
+            <iframe
+              width="853"
+              height="480"
+              src={`https://www.youtube.com/embed/${item.video_url}`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="Embedded youtube"
+            />
           );
         } else {
           return <p>Nothing to see here...</p>;
